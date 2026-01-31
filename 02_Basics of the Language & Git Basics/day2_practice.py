d5c6ff4eb9f52d4ef8cdcd9375980174ad6f15a6
Montana Katz <montanakatz64@gmail.com>
2026-01-31 19:04:51 +0100

    Ajouts d'exercices supplémentaires

--- 02_Basics of the Language & Git Basics/day2_practice.py
+++ 02_Basics of the Language & Git Basics/day2_practice.py
@@ -1,3 +1,7 @@
+# Disclaimer : j'écris en français ou en anglais pour ce challenge mais habituellement je choisis qu'une seule des deux langues pour mes projets. Ce choix est dû à une volonté de m'entraîner dans les deux langues.
+
+# Types : int, float, str, bool
+
 print ("Welcome to Data Science! Let's (re)explore Python syntax together.")
 
 name = "bob"
@@ -5,3 +9,15 @@ age = 20
 favorite_number =  16
 
 print(f"His name is {name}, he's {age} years old and his favorite number is {favorite_number}")
+
+# Mes exercices
+
+pomme = 1
+poire = 2
+fruits = pomme + poire
+fruits = float(fruits)
+
+print(fruits)
+
+my_str = "hello"
+
