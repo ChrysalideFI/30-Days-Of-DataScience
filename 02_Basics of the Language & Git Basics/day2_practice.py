91190cc3d92b2b5aa6eb0faebdf5618879f948fa
Montana Katz <montanakatz64@gmail.com>
2026-01-31 18:56:45 +0100

    Practice of day 2

---
+++ 02_Basics of the Language & Git Basics/day2_practice.py
@@ -0,0 +1,7 @@
+print ("Welcome to Data Science! Let's (re)explore Python syntax together.")
+
+name = "bob"
+age = 20
+favorite_number =  16
+
+print(f"His name is {name}, he's {age} years old and his favorite number is {favorite_number}")
