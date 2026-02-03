b691a46e42e6d239d685600e5b803a663074c353
Montana Katz <montanakatz64@gmail.com>
2026-02-04 00:59:44 +0100

    Creation of a math module for arithmetic operation, exercise 3 day 4

---
+++ 04_Functions and Modular Programming/math_utils.py
@@ -0,0 +1,27 @@
+def add(number_1, number_2):
+  result = number_1 + number_2
+  return result
+
+def subtraction(number_1, number_2):
+  result = number_1 - number_2
+  return result
+
+def multiplication(number_1, number_2):
+  result = number_1 * number_2
+  return result
+
+def division(number_1, number_2):
+  result = number_1 / number_2
+  return result
+
+def integer_division(number_1, number_2): # division entière : arrondit à l'entier inférieur
+  result = number_1 // number_2
+  return result
+
+def modulo(number_1, number_2):
+  result = number_1 % number_2
+  return result
+
+def exponentiation(number_1, number_2):
+  result = number_1 ** number_2
+  return result
